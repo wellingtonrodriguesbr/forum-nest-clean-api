1fa88d4d1785689bd01cc5182a00f8b163fbabc3
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2025-10-11 09:30:19 -0300

    fix: reorder import statement for StorageModule in HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -38,8 +38,8 @@ import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cas
 import { FetchAnswerCommentsController } from "./controllers/fetch-answer-comments.controller";
 import { FetchAnswerCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-answer-comments";
 import { UploadAttachmentController } from "./controllers/upload-attachment.controller";
-import { StorageModule } from "../storage/storage.module";
 import { UploadAndCreateAttachmentUseCase } from "@/domain/forum/application/use-cases/upload-and-create-attachment-use-case";
+import { StorageModule } from "../storage/storage.module";
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, StorageModule],
